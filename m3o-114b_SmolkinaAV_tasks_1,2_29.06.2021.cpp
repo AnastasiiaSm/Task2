4e90664b9ce4e13a706bec9e4632ecde93993cbb
AnastasiiaSm <smolkinan80@gmail.com>
2021-06-30 16:11:20 +0300

    first ver

---
+++ m3o-114b_SmolkinaAV_tasks_1,2_29.06.2021.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+//Задание 1
+//Вариант 4:
+//Написать удаления со сдвигом влево n-го элемента массива. Пустой элемент справа заполняется нулями.
+
+void deleteFromMas(int* source,int n, int size) {
+	for (int i = n-1; i < size-1; i++) {
+		source[i] = source[i+1];
+	}
+	source[size-1] = 0;
+	for (int i = 0; i < size; i++) {
+		std::cout << source[i] << " ";
+	}
+}
+
+
+int main()
+{
+	int a,b;
+	std::cout << "Введите размер массива:";
+	std::cin >> a;
+	std::cout << "Введите элемент, который нужно удалить:";
+	std::cin >> b;
+	test (b, a);
+	int arr[a];
+	std::cout << "Введите массив:" << std::endl;
+	for (int i = 0; i < a; i++) {
+		std::cin >> arr[i];
+	}
+	deleteFromMas(arr, b, a);
+	return 0;
+}
